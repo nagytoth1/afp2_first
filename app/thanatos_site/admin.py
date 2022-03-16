8f1beca3982a8b0a2265cbbb12cae69a69c9ce9d
dzkbx0 <dzkbx0@std.uni-eszterhazy.hu>
2022-03-16 19:25:11 +0100

    interface elaborated, new model: TaskType
    Kialakítottam az oldal arculatát, új modelt hoztam létre Type néven (feladattípusokat fogja meghatározni)

--- app/thanatos_site/admin.py
+++ app/thanatos_site/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from . models import Subject
-from . models import Task
+from . models import Subject, Type, Task
 
 admin.site.register(Subject)
 admin.site.register(Task)
+admin.site.register(Type)
--- app/thanatos_site/views.py
+++ app/thanatos_site/views.py
@@ -1,14 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from . models import Subject
+from django.core.exceptions import ObjectDoesNotExist
 
 def mainPage(request):
     subjects = Subject.objects.all() # összes adatot lekérjük (Select *)
     
-    queryparam_subject = request.GET.get('subject')
+    queryparam_subject = request.GET.get('s')
     
+    #ha még nincs megadva subject query-paraméterként (s nincs beállítva vagy üres), akkor a főoldalt dobja ki
     if queryparam_subject is None or queryparam_subject == '':
         return render(request, 'main.html',{'subjects_names':subjects})
+    #ha be van állítva az s értéke, akkor tudjuk, hogy melyik tárgyat kell lekérni
     else:
-        subject_data:Subject = Subject.objects.get(pk=queryparam_subject)
+        try:
+            subject_data:Subject = Subject.objects.get(pk=queryparam_subject)
+        #ha nem létezik, pl. s=32 van beállítva, akkor kivételkezelés, dobjon vissza a főoldalra
+        except ObjectDoesNotExist: 
+            return redirect('home')
         return HttpResponse(f'<h1>{subject_data.subject_name}</h1>')
