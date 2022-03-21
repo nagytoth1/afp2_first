1c3a8a4ac81a1b53118d5562b3ae8b726fe4fac8
krisztian <hegyikrisztian1@gmail.com>
2022-03-21 17:16:33 +0100

    dynamically loads tasks based on subject selection

--- app/thanatos_site/views.py
+++ app/thanatos_site/views.py
@@ -2,11 +2,12 @@ from audioop import reverse
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from . models import Subject
+from . models import Task
 from django.core.exceptions import ObjectDoesNotExist
 
 def mainPage(request):
     subjects = Subject.objects.all() # összes adatot lekérjük (Select *)
-    
+    subject_tasks = Task.objects.all() #az összes feladat lekérése későbbi szűrésre
     queryparam_subject = request.GET.get('id')
     
     #ha még nincs megadva subject query-paraméterként (s nincs beállítva vagy üres), akkor a főoldalt dobja ki
@@ -14,9 +15,11 @@ def mainPage(request):
         return render(request, 'main.html',{'subjects_names':subjects})
     #ha be van állítva az s értéke, akkor tudjuk, hogy melyik tárgyat kell lekérni
     else:
+        curr_subject = subjects.filter(id__exact=queryparam_subject) #lekérem id alapján a kiválasztott tárgyat
+        filtered_tasks = subject_tasks.filter(subject__exact=curr_subject.first()) # a first()-el megkapom a tárgy nevét stringként és arra szűrök
         try:
             subject_data:Subject = Subject.objects.get(pk=queryparam_subject)
         #ha nem létezik, pl. s=32 van beállítva, akkor kivételkezelés, dobjon vissza a főoldalra
         except ObjectDoesNotExist: 
             return redirect('home')
-        return render(request, 'tasks.html')
+        return render(request, 'tasks.html', {'tasks':filtered_tasks})
